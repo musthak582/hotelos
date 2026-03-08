64eaf037f1dfac85047c24a8f8101fb786fbbb3b
musthak582 <musthakmusthak582@gmail.com>
2026-03-08 16:09:30 +0530

    fix-auth-ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -51,10 +51,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
     Credentials({
       name: "credentials",
       credentials: {
